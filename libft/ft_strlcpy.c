dffe0e6dbe6e045320c848d62126453cc6e3c2a9
k2k <zootutoro@gmail.com>
2025-04-08 22:44:47 +0900

    ft_strlcpy done

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -3,4 +3,31 @@
 
 size_t strlcpy(char *dest, const char *restrict src, size_t dsize)
 {
+	size_t i;
+	size_t size_s;
+
+	size_s = 0;
+	while (src[size_s])
+		size_s++;
+
+	i = 0;
+	if (size_s < dsize)
+	{
+		while (i < size_s)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (i < dsize - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	dest[i] = '\0';
+	return size_s;
+}
 
---
+++ libft/strlcat.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include <stdio.h>
+
+size_t strlcat(char *dest, const char *restrict src, size_t dsize)
+{
+
