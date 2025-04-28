c23985524d8165371389629da168e3a41a72b896
k2k <zootutoro@gmail.com>
2025-04-28 19:56:26 +0900

    not done ft_strtirm

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,52 +6,59 @@
 /*   By: kkono <kkono@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 09:32:04 by kkono             #+#    #+#             */
-/*   Updated: 2025/04/26 09:32:35 by kkono            ###   ########.fr       */
+/*   Updated: 2025/04/28 19:51:43 by kkono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	int		set_check;
-	int		s1_start;
-	int		s1_last;
-	char	*strtrim;
+int is_inset(char s1, char const *set);
 
-	set_check = 0;
-	s1_start = 0;
-	s1_last = 0;
-	while (s1[s1_last])
-		s1_last++;
-	while (set[set_check])
-	{
-		if (set[set_check] == s1[s1_start])
-		{
-			s1_start++;
-			set_check = 0;
-		}
-		else
-			set_check++;
-	}
-	set_check = 0;
-	while (set[set_check])
-	{
-		if (set[set_check] == s1[s1_last])
-		{
-			s1_last++;
-			set_check = 0;
-		}
-		else
-			set_check++;
-	}
-	strtrim = (char *)malloc(sizeof(char) * (s1_last - s1_start + 1));
-	if (!strtrim)
+char *ft_strtrim(char const *s1, char const *set)
+{
+	int first;
+	int last;
+	int i;
+	char *str_trimmed;
+
+	if (!s1||!set)
 		return (NULL);
-	while (s1_start < s1_last)
-	{
-		strtrim[s1_start] = s1[s1_start];
-		s1_start++;
-	}
-	return (strtrim);
+	i = 0;
+	first = 0;
+	last = ft_strlen(s1) - 1;
+	while (is_inset(s1[first], set))
+		first++;
+	while (is_inset(s1[last], set))
+		last--;
+	str_trimmed = (char *)malloc(sizeof(char) * (last - first + 2));
+	if (!str_trimmed)
+		return (NULL);
+	while (last >= first)
+		str_trimmed[i++] = s1[first++];
+	str_trimmed[i] = '\0';
+	return (str_trimmed);
 }
+
+int is_inset(char s1, char const *set)
+{
+	int i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (s1 == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+// int main(void)
+// {
+// 	char *ans;
+// 	char const *s1 = "   xxxtripouille";
+// 	char const *set = " x";
+// 	ans = ft_strtrim(s1,set);
+// 	printf("%s\n", ans);
+// 	free(ans);
+// 	return (0);
+// }
